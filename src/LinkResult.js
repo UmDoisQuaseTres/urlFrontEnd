2d02d4310e45b7da99528e0a068508422540714f
Matheus de Oliveira <um.dois.quase.tres@gmail.com>
2022-04-28 01:06:44 -0300

    Fist commit

---
+++ src/LinkResult.js
@@ -0,0 +1,64 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+import CopyToClipboard from "react-copy-to-clipboard";
+
+const LinkResult = ({ inputValue }) => {
+  const [shortenLink, setShortenLink] = useState("");
+  const [copied, setCopied] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const fetchData = async () => {
+    const res = await axios.post('https://shorturlshort.herokuapp.com/new',{
+        url:`${inputValue}`
+      })
+    try {
+      setLoading(true);
+      setShortenLink(res.data.code);
+    } catch(err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    if(inputValue.length) {
+      fetchData();
+    }
+  }, [inputValue]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [copied]);
+
+  if(loading) {
+    return <p className="noData">Loading...</p>
+  }
+  if(error) {
+    return <p className="noData">Something wne t wrong :(</p>
+  }
+const domain = process.env.DOMAIN;
+
+  return (
+    <>
+      {shortenLink && (
+        <div className="result">
+          <p>{domain + shortenLink}</p>
+          <CopyToClipboard
+            text={shortenLink}
+            onCopy={() => setCopied(true)}
+          >
+            <button className={copied ? "copied" : ""}>Copy to Clipboard</button>
+          </CopyToClipboard>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default LinkResult
